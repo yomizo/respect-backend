f2baa73f646933aed51e3855e54430b936d06839
make a splash <macbook@Mac-mini.local>
2019-05-14 23:38:36 +0900

    try cors.rb 'http://111.107.49.106:8080'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://111.107.49.106:8080'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true,
+      expose: ['Authorization']
   end
 end
