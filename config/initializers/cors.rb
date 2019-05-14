5e94091c66e5bdc7b373c6cbb0aaee098fa50dff
make a splash <macbook@Mac-mini.local>
2019-05-15 01:15:37 +0900

    add 'Access-Controll-Allow-Origin' in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true,
-      expose: ['Authorization']
+      expose: ['Authorization', 'Access-Controll-Allow-Origin']
   end
 end
